9120049dff126f693b7229ee7880d9ce5a72b6c7
Steven Fisher <steven.fisher22@gmail.com>
2019-04-22 22:36:23 -0500

    comments

--- rewind-movie-review-api/controllers/movies.js
+++ rewind-movie-review-api/controllers/movies.js
@@ -1,3 +1,6 @@
+// Controllers define routes and business logic 
+// Controllers call to repositories when you need to access the database
+
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
--- rewind-movie-review-api/server.js
+++ rewind-movie-review-api/server.js
@@ -2,8 +2,8 @@
 // And is where you use middleware and routes
 
 // Express 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Port 
 const port = 3030;
@@ -17,13 +17,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(cors())
-
 app.use(bodyParser.json())
 
+// Routes
 app.get('/', (req, res) => res.send('Hello World!'));
-
-app.use('/movies', movieRoutes);
-
-app.use('/users', userRoutes)
+app.use('/movies', movieRoutes); // From Controllers
+app.use('/users', userRoutes) // From Controllers
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
